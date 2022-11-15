2174eed72a063d015d167de18ac91a47f37c42ad
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-11-15 22:07:00 +0300

    add

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import music_data from "./src/music-data.json";
+import SongCard from "./src/components/SongCard/SongCard";
+import SearchBar from "./src/components/SearchBar/SearchBar";
 
 export default function App() {
+  const [list, setList] = useState(music_data);
+  const renderSong = ({ item }) => <SongCard song={item} />;
+  const renderSeperator = () => <View style={styles.seperator} />;
+  const handleSearch = (text) => {
+    const filteredList = music_data.filter((song) => {
+      const searcedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+      return currentTitle.indexOf(searcedText) > -1;
+    });
+
+    setList(filteredList);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SearchBar onSearch={handleSearch} />
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={list}
+        renderItem={renderSong}
+        ItemSeparatorComponent={renderSeperator}
+      />
     </View>
   );
 }
@@ -13,8 +33,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  seperator: {
+    borderWidth: 1,
+    borderColor: "#eceff1",
   },
 });
---
+++ src/components/SearchBar/SearchBar.styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: "#eceff1",
+    padding: 18,
+    margin: 15,
+    borderRadius: 5,
+    fontSize: 20,
+  },
+});
